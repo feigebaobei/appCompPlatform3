894a563d808691fb3f5494f13eed7137b82101f4
laodingdang <13269735167@163.com>
2018-05-15 15:37:23 +0800

    end

--- src/components/compOverview/compBackupsEdit.vue
+++ src/components/compOverview/compBackupsEdit.vue
@@ -95,13 +95,11 @@ export default {
   methods: {
     // 编辑提交
     handleSubmitAndAlert (name) {
-      console.log('1', this.formItem)
       this.$refs[name].validate((valid) => {
         if (valid) {
-        console.log('2', this.formItem)
           this.$axios({
             method: 'post',
-            url: 'http://infra.xesv5.com//api/backup/edit?token=' + this.getRequest().token,
+            url: 'http://infra.xesv5.com/api/backup/edit?token=' + this.getRequest().token,
             data: this.qs.stringify({
               name: this.formItem.name,
               type: this.formItem.policy_type_group,
@@ -163,11 +161,10 @@ export default {
       this.editData = res.data.data
       this.formItem.name = this.editData.name
       this.formItem.application_group = this.editData.application_id
-      this.formItem.period = this.editData.period
       this.formItem.policy_type_group = this.editData.policy_type
       this.formItem.instance_id = this.editData.instance_id
       this.formItem.target_group = this.editData.target_type
-      this.formItem.time_group = parseInt(this.editData.period)
+      this.formItem.time_group = this.editData.period
     })
   }
 }
